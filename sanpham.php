17ce2ec75e0d5ba6a39d8a3b54aadc4fdfb4f621
longchen204 <150640060+longchen204@users.noreply.github.com>
2023-12-01 09:52:58 +0700

    Add files via upload

---
+++ sanpham.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"></script>
+    <link rel="stylesheet" href="sanpham.css">
+    <title>Document</title>
+</head>
+<body>
+        <header>
+            <div class="header1">
+                <img src="image/Ellipse 2.png" alt=""> 
+                <h2>PetPalace</h2>
+                <div class="search">
+                    <input type="text" placeholder="Tìm Kiếm Sản Phẩm">
+                    <div>
+                        <i class="fa-solid fa-magnifying-glass fa-2xl"></i>
+                    </div>
+                </div>
+                <a  id="dn" href=""><input type="button" value="Đăng Nhập"></a>
+                <a id="dk" href=""><input type="button" value="Đăng Ký"></a>
+                <div class="user">
+                    <i class="fa-regular fa-user fa-2xl"></i>
+                    <a href="">Tên người dùng</a>
+                </div>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="index.html">Trang Chủ</a></li>
+                    <li><a href="chocanh.html">Giống Chó Cảnh</a></li>
+                    <li><a href="meocanh.html">Giống Mèo Cảnh</a></li>
+                    <li><a href="spa.html">Spa & Groomimg</a></li>
+                    <li><a href="">Phụ Kiện Pet</a></li>
+                </ul>
+                <div class="giohang">
+                    <i class="fa-solid fa-cart-plus fa-xl"></i>
+                </div>
+                <h3>Liên hệ : 035678908</h3>
+            </div>
+        </header>
+    <main>
+        <div id="wrapper">
+            <div class="headline">
+                <h3>Sản phẩm bán chạy</h3>
+            </div>
+        </div>
+        <ul class="products">
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="image/cho-bully.jpg" alt="">
+                        </a>
+                        <a href="" class="buy-now">mua ngay</a>
+                    </div>
+                    <div class="product-info">
+                        <a href="" class="product-cat">Chó</a>
+                        <a href="" class="product-name">Chó Bully cảnh đẹp</a>
+                        <div class="product-price">25.000.000đ</div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="image/cho-beagle.png" alt="">
+                        </a>
+                        <a href="" class="buy-now">mua ngay</a>
+                    </div>
+                    <div class="product-info">
+                        <a href="" class="product-cat">Chó</a>
+                        <a href="" class="product-name">Chó Bully cảnh đẹp</a>
+                        <div class="product-price">25.000.000đ</div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="image/cho-corgi-1.jpg" alt="">
+                        </a>
+                        <a href="" class="buy-now">mua ngay</a>
+                    </div>
+                    <div class="product-info">
+                        <a href="" class="product-cat">Chó</a>
+                        <a href="" class="product-name">Chó Bully cảnh đẹp</a>
+                        <div class="product-price">25.000.000đ</div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="image/cho-pug-mat-xe.jpg" alt="">
+                        </a>
+                        <a href="" class="buy-now">mua ngay</a>
+                    </div>
+                    <div class="product-info">
+                        <a href="" class="product-cat">Chó</a>
+                        <a href="" class="product-name">Chó Bully cảnh đẹp</a>
+                        <div class="product-price">25.000.000đ</div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </main>
+</body>
+</html>
